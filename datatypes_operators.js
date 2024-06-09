9834d51eb53a36f6be4feebdf4203960f859c51d
Wis3477 <mabohlalenkuna@gmail.com>
2024-06-09 03:12:49 +0200

    first

---
+++ datatypes_operators.js
@@ -0,0 +1,49 @@
+//Exercise 1: Numbers
+let num1 = 12;
+let num2 = 10.5;
+
+sum = num1 + num2;
+difference = num1 - num2;
+product = num1 * num2;
+quotient = num1 / num2;
+
+console.log("Sum is:", sum);
+console.log("Difference is:", difference);
+console.log("Product is:", product);
+console.log("Quotient is:", quotient);
+
+//Exercise 2: Boolean and Operators
+let numb1 = 5;
+let numb2 = 10;
+
+let isGreater = numb1 > numb2;
+let isLessOrEqual = numb1 <= numb2;
+let isEqual = numb1 === numb2;
+let isNotEqual = numb1 !== numb2;
+
+let x = 8;
+let y = 12;
+console.log(x > y);
+console.log( x <= y);
+console.log( x === y);
+console.log( x !== y);
+
+let a = true;
+let b = false;
+
+console.log("a AND b:", a && b);
+console.log("a OR b:", a || b);
+console.log("NOT a:", a | b);
+
+let p =10;
+p += 5;
+console.log(p);
+p -= 8;
+console.log(p);
+p *= 5;
+console.log(p);
+p /= 10;
+console.log(p);
+p %= 3;
+console.log(p);
+
---
+++ variables.js
@@ -0,0 +1,22 @@
+//Exercise 1: Variable Declaration
+let name = "Wise";
+let age = 24;
+let isStudent = true;
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+//Exercise 2: Variable Reassignment
+let favoriteColour = "black";
+console.log("Favorite Colour:", favoriteColour);
+favoriteColour = "white";
+console.log("Favorite Colour:", favoriteColour);
+
+//Exercise 3: Arithmetic Operations
+let num1=10, num2=5;
+let sum, difference, product, quotient;
+
+console.log("Sum is:", sum);
+console.log("Difference is:", difference);
+console.log("Product is:", product);
+console.log("Quotient is:", quotient);
